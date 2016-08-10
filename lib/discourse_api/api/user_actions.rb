b0ab08198b66188edd95407802d3f8c22a1d2ac0
PJ Davis <pj.davis@gmail.com>
2016-08-10 12:23:10 -0500

    add topics and replies

--- lib/discourse_api/api/user_actions.rb
+++ lib/discourse_api/api/user_actions.rb
@@ -6,6 +6,12 @@ module DiscourseApi
         response = get("/user_actions.json", params)
         response.body["user_actions"]
       end
+
+      def user_topics_and_replies(username)
+        params = {"username": username, "filter": "4,5"}
+        response = get("/user_actions.json", params)
+        response.body["user_actions"]
+      end
     end
   end
 end
--- spec/discourse_api/api/user_actions_spec.rb
+++ spec/discourse_api/api/user_actions_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe DiscourseApi::API::UserActions do
   subject { DiscourseApi::Client.new("http://localhost:3000", "test_d7fd0429940", "test_user") }
 
-  describe "#replies" do
+  describe "#user_replies" do
     before do
       stub_get("http://localhost:3000/user_actions.json?api_key=test_d7fd0429940&api_username=test_user&username=testuser&filter=5").to_return(body: fixture("replies.json"), headers: { content_type: "application/json" })
     end
@@ -15,6 +15,22 @@ describe DiscourseApi::API::UserActions do
 
     it "returns the requested user" do
       replies = subject.user_replies("testuser")
+      expect(replies).to be_an Array
+    end
+  end
+
+  describe "#user_topics_and_replies" do
+    before do
+      stub_get("http://localhost:3000/user_actions.json?api_key=test_d7fd0429940&api_username=test_user&username=testuser&filter=4,5").to_return(body: fixture("replies_and_topics.json"), headers: { content_type: "application/json" })
+    end
+
+    it "requests the correct resource" do
+      subject.user_topics_and_replies("testuser")
+      expect(a_get("http://localhost:3000/user_actions.json?api_key=test_d7fd0429940&api_username=test_user&username=testuser&filter=4,5")).to have_been_made
+    end
+
+    it "returns the requested user" do
+      replies = subject.user_topics_and_replies("testuser")
       expect(replies).to be_an Array
     end
   end
